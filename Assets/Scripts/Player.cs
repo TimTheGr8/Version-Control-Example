7ea4b38bf24711218bd6893f3481e04c35be32ac
TimTheGr8 <tjanssen4@gmail.com>
2023-04-10 21:04:58 -0500

    Added the scripts folder ans started working on the player script.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
